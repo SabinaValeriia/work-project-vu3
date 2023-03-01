902b457de1a648ffea76c1750a57a10925f5d4bc
Valeriia Sabina <valeriia.sabina@onix-systems.com>
2023-03-01 11:11:30 +0200

    t1 - Add changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,36 +1,51 @@
 <template lang="pug">
 .login 
-  .login-block 
+  #login.login-block 
     img(src="../assets/logo.svg")
     .login-block__box
-      h1 Get started with a 14 day free trial
-      h2 Already have an account?
+      h1 Log in to your Quentn account
+      h2 Not a member?
         span 
-          a Log in
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input(type="text", v-model="v$.name.$model", placeholder="Name")
-      span.error(v-for="(error, index) in v$.name.$errors", :key="index") {{ error.$message }}
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input(type="text", v-model="v$.email.$model", placeholder="Email")
-      span.error(v-for="(error, index) in v$.email.$errors", :key="index") {{ error.$message }}
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input#password(
-          type="password",
-          v-model="v$.password.$model",
-          placeholder="Password"
+          a(href="/registration", target="_blank") Sign up
+      form
+        .input-group(
+          :class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }"
         )
-      button(@click="switchVisibility") show / hide
-      .login-block__error
-        p#lowercase.password One lowercase character
-        p#uppercase.password One uppercase character
-      .login-block__error
-        p#number.password One number
-        p#min.password 8 characters minimum
-      span.password(
-        v-for="(error, index) in v$.password.$errors",
-        :key="index"
-      ) {{ error.$message }}
-      button Create an account
+          label.hide(for="InputEmail") Email
+          input.form-control.form--input(
+            type="text",
+            v-model="v$.email.$model",
+            placeholder="Email"
+          )
+          span.error(v-for="(error, index) in v$.email.$errors", :key="index") {{ error.$message }}
+        .input-group(
+          :class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }"
+        )
+          input#password.form-control.form--input(
+            type="password",
+            v-model="v$.password.$model",
+            placeholder="Password"
+          )
+          span.error(
+            v-for="(error, index) in v$.password.$errors",
+            :key="index"
+          ) {{ error.$message }}
+          button.password(@onclick.prevent="switchVisibility()") Show
+
+        button(@click.prevent="addExternalResults()") Create an account
+  #confirm.login-block.hide
+    img(src="../assets/logo.svg")
+    .login-block__box.login-block__confirm
+      h1 Confirm your email
+      p Please check your inbox for a confirmation email. Click the link in the email to confirm your email address.
+      button(@click.prevent="resendLink()") Re-send confirmation link
+  #login-page.login-block.hide
+    img(src="../assets/logo.svg")
+    .login-block__box.login-block__confirm
+      h1 Congratulations!
+      p Your email has been confirmed. You can now login to the system
+      button
+        a(href="/login") Go to Login
   .login-bg
 </template>
 
@@ -38,15 +53,6 @@
 import { ref } from "vue";
 import useVuelidate from "@vuelidate/core";
 import { email } from "@vuelidate/validators";
-const passwordField = document.querySelector("#password");
-
-const switchVisibility = () => {
-  if (passwordField != null) {
-    if (passwordField.getAttribute("type") === "password")
-      passwordField.setAttribute("type", "text");
-    else passwordField.setAttribute("type", "password");
-  }
-};
 const validName = (name: string) => {
   let validNamePattern = new RegExp("^[a-zA-Z]+(?:[-'\\s][a-zA-Z]+)*$");
   if (validNamePattern.test(name)) {
@@ -107,44 +113,115 @@ const minLength = (password: string) => {
     return false;
   }
 };
-const rules = {
-  name: {
+let rules = {
+  email: {
+    email,
     name_validation: {
-      $validator: validName,
       $message: "Incorrect format",
     },
   },
-  email: {
-    email,
-    $message: "Incorrect format",
-  },
   password: {
-    name_validation: {
-      $validator: containsUppercase,
-    },
-    name_validationS: {
-      $validator: containsLowercase,
-    },
-    name_validationT: {
-      $validator: containsNumber,
-    },
     name_validationF: {
       $validator: minLength,
+      $message: "Incorrect format",
     },
   },
 };
 
 const form = ref({
-  name: "",
   email: "",
-  repeatedPassword: null,
-  password: null,
-  repeatedPasswordEl: "",
-  isEmailTouched: "",
+  password: "",
 });
-const v$ = useVuelidate(rules, form);
+const $externalResults = ref({});
+
+const v$ = useVuelidate(rules, form, { $externalResults });
+
+const addExternalResults = (e: {
+  [x: string]: any;
+  preventDefault: () => void;
+}) => {
+  if (!form.value.email) {
+    $externalResults.value = {
+      email: ["Require"],
+    };
+    v$.value.$validate();
+  } else {
+    var element = document.getElementById("login");
+
+    document.getElementById("login")?.classList.add("hide");
+    document.getElementById("confirm")?.classList.remove("hide");
+    document.getElementById("confirm")?.classList.add("block");
+  }
+};
+const resendLink = () => {
+  document.getElementById("confirm")?.classList.add("hide");
+  document.getElementById("login-page")?.classList.add("block");
+};
 </script>
 <style lang="scss" scoped>
+#login-page.login-block.block {
+  display: block;
+}
+#confirm.login-block.block {
+  display: block;
+}
+#confirm.login-block.hide {
+  display: none;
+}
+.hide {
+  display: none;
+}
+.input-group {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  max-height: 56px;
+  min-height: 56px;
+  &:nth-child(2) {
+    margin: 40px 0 40px 0;
+  }
+
+  label.hide {
+    display: none;
+    margin-bottom: -30px;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    color: var(--dark-charcoal);
+    &.error {
+      display: block;
+    }
+  }
+  .input-group label:focus {
+    position: absolute;
+    display: block;
+    bottom: 28px;
+  }
+
+  .form--input {
+    padding: 0;
+
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #000000;
+    border-radius: unset;
+    outline: none;
+    box-shadow: none;
+
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 28px;
+    color: var(--cod-gray);
+
+    &::placeholder {
+      color: var(--dark-charcoal);
+    }
+  }
+}
+
 .login {
   justify-content: space-between;
   display: flex;
@@ -201,6 +278,27 @@ const v$ = useVuelidate(rules, form);
       font-weight: 600;
       font-size: 18px;
       line-height: 24px;
+      margin-top: 40px;
+      a {
+        color: #fff;
+      }
+    }
+    button.password {
+      margin-top: 0;
+      font-family: "Poppins";
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 24px;
+      text-align: right;
+      color: #00aed8;
+      border: none;
+      background: transparent;
+      position: absolute;
+      top: 10px;
+      left: 361px;
+      cursor: pointer;
+      width: 100px;
     }
     span.error {
       font-family: "Outfit";
@@ -245,6 +343,9 @@ const v$ = useVuelidate(rules, form);
     }
     &__error {
       display: flex;
+    }
+    &__confirm {
+      padding-top: 230px;
     }
   }
 }
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,36 +1,68 @@
 <template lang="pug">
 .login 
-  .login-block 
+  #login.login-block 
     img(src="../assets/logo.svg")
+    //- img(:src="require(`@/assets/logo.svg)`)")
     .login-block__box
       h1 Get started with a 14 day free trial
       h2 Already have an account?
         span 
-          a Log in
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input(type="text", v-model="v$.name.$model", placeholder="Name")
-      span.error(v-for="(error, index) in v$.name.$errors", :key="index") {{ error.$message }}
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input(type="text", v-model="v$.email.$model", placeholder="Email")
-      span.error(v-for="(error, index) in v$.email.$errors", :key="index") {{ error.$message }}
-      div(:class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }")
-        input#password(
-          type="password",
-          v-model="v$.password.$model",
-          placeholder="Password"
+          a(href="/login", target="_blank") Log in
+      form
+        .input-group(
+          :class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }"
         )
-      button(@click="switchVisibility") show / hide
-      .login-block__error
-        p#lowercase.password One lowercase character
-        p#uppercase.password One uppercase character
-      .login-block__error
-        p#number.password One number
-        p#min.password 8 characters minimum
-      span.password(
-        v-for="(error, index) in v$.password.$errors",
-        :key="index"
-      ) {{ error.$message }}
-      button Create an account
+          label.hide(for="InputEmail") Name
+          input.form-control.form--input(
+            type="text",
+            v-model="v$.name.$model",
+            placeholder="Name"
+          )
+          span.error(v-for="(error, index) in v$.name.$errors", :key="index") {{ error.$message }}
+        .input-group(
+          :class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }"
+        )
+          label.hide(for="InputEmail") Email
+          input.form-control.form--input(
+            type="text",
+            v-model="v$.email.$model",
+            placeholder="Email"
+          )
+          span.error(v-for="(error, index) in v$.email.$errors", :key="index") {{ error.$message }}
+        .input-group(
+          :class="{ valid: !v$.$error && v$.$dirty, error: v$.$error }"
+        )
+          input#password.form-control.form--input(
+            :type="[showPassword ? 'text' : 'password']",
+            v-model="v$.password.$model",
+            placeholder="Password"
+          )
+          button.password(@click.prevent="showPassword = !showPassword") Show
+        .login-block__error
+          p#lowercase.password One lowercase character
+          p#uppercase.password One uppercase character
+        .login-block__error
+          p#number.password One number
+          p#min.password 8 characters minimum
+        span.password(
+          v-for="(error, index) in v$.password.$errors",
+          :key="index"
+        ) {{ error.$message }}
+        button(@click.prevent="addExternalResults()") Create an account
+        .static(:class="{ active: isActive, 'text-danger': hasError }")
+  #confirm.login-block.hide
+    img(src="../assets/logo.svg")
+    .login-block__box.login-block__confirm
+      h1 Confirm your email
+      p Please check your inbox for a confirmation email. Click the link in the email to confirm your email address.
+      button(@click.prevent="resendLink()") Re-send confirmation link
+  #login-page.login-block.hide
+    img(src="../assets/logo.svg")
+    .login-block__box.login-block__confirm
+      h1 Congratulations!
+      p Your email has been confirmed. You can now login to the system
+      button
+        a(href="/login") Go to Login
   .login-bg
 </template>
 
@@ -38,15 +70,9 @@
 import { ref } from "vue";
 import useVuelidate from "@vuelidate/core";
 import { email } from "@vuelidate/validators";
-const passwordField = document.querySelector("#password");
-
-const switchVisibility = () => {
-  if (passwordField != null) {
-    if (passwordField.getAttribute("type") === "password")
-      passwordField.setAttribute("type", "text");
-    else passwordField.setAttribute("type", "password");
-  }
-};
+const showPassword = ref(false);
+const isActive = ref(true);
+const hasError = ref(false);
 const validName = (name: string) => {
   let validNamePattern = new RegExp("^[a-zA-Z]+(?:[-'\\s][a-zA-Z]+)*$");
   if (validNamePattern.test(name)) {
@@ -107,7 +133,7 @@ const minLength = (password: string) => {
     return false;
   }
 };
-const rules = {
+let rules = {
   name: {
     name_validation: {
       $validator: validName,
@@ -116,7 +142,9 @@ const rules = {
   },
   email: {
     email,
-    $message: "Incorrect format",
+    name_validation: {
+      $message: "Incorrect format",
+    },
   },
   password: {
     name_validation: {
@@ -137,14 +165,95 @@ const rules = {
 const form = ref({
   name: "",
   email: "",
-  repeatedPassword: null,
-  password: null,
-  repeatedPasswordEl: "",
-  isEmailTouched: "",
+  password: "",
 });
-const v$ = useVuelidate(rules, form);
+const $externalResults = ref({});
+
+const v$ = useVuelidate(rules, form, { $externalResults });
+
+const addExternalResults = () => {
+  if (!form.value.name) {
+    $externalResults.value = {
+      email: ["Required"],
+    };
+    v$.value.$validate();
+  } else {
+    var element = document.getElementById("login");
+
+    document.getElementById("login")?.classList.add("hide");
+    document.getElementById("confirm")?.classList.remove("hide");
+    document.getElementById("confirm")?.classList.add("block");
+  }
+};
+const resendLink = () => {
+  document.getElementById("confirm")?.classList.add("hide");
+  document.getElementById("login-page")?.classList.add("block");
+};
 </script>
 <style lang="scss" scoped>
+#login-page.login-block.block {
+  display: block;
+}
+#confirm.login-block.block {
+  display: block;
+}
+#confirm.login-block.hide {
+  display: none;
+}
+.hide {
+  display: none;
+}
+.input-group {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  max-height: 56px;
+  min-height: 56px;
+  &:nth-child(2) {
+    margin: 40px 0 40px 0;
+  }
+
+  label.hide {
+    display: none;
+    margin-bottom: -30px;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    color: var(--dark-charcoal);
+    &.error {
+      display: block;
+    }
+  }
+  .input-group label:focus {
+    position: absolute;
+    display: block;
+    bottom: 28px;
+  }
+
+  .form--input {
+    padding: 0;
+
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #000000;
+    border-radius: unset;
+    outline: none;
+    box-shadow: none;
+
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 28px;
+    color: var(--cod-gray);
+
+    &::placeholder {
+      color: var(--dark-charcoal);
+    }
+  }
+}
+
 .login {
   justify-content: space-between;
   display: flex;
@@ -201,6 +310,27 @@ const v$ = useVuelidate(rules, form);
       font-weight: 600;
       font-size: 18px;
       line-height: 24px;
+      margin-top: 40px;
+      a {
+        color: #fff;
+      }
+    }
+    button.password {
+      margin-top: 0;
+      font-family: "Poppins";
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 24px;
+      text-align: right;
+      color: #00aed8;
+      border: none;
+      background: transparent;
+      position: absolute;
+      top: 10px;
+      left: 361px;
+      cursor: pointer;
+      width: 100px;
     }
     span.error {
       font-family: "Outfit";
@@ -245,6 +375,9 @@ const v$ = useVuelidate(rules, form);
     }
     &__error {
       display: flex;
+    }
+    &__confirm {
+      padding-top: 230px;
     }
   }
 }
