0504e50625eb22baae7be6cb70f7c14bdc296e7f
Valeriia Sabina <valeriia.sabina@onix-systems.com>
2023-03-16 11:12:57 +0200

    t2 - Add component pagination

--- src/components/PaginationList.vue
+++ src/components/PaginationList.vue
@@ -1,89 +1,16 @@
 <template lang="pug">
 .pagination
-  div(v-for="item in paginatedData", :key="item.index") 
+  div(v-for="item in paginatedData", :key="item.index")
     div(v-if="item.value") {{ item.value }}
-  button.prev(@click="backPage") Prev
-  button.item(
-    v-for="item in Math.ceil(dataArray.length / perPage)",
-    :key="item",
-    @click="() => goToPage(item)"
-  ) {{ item }}
-  button.next(@click="nextPage") Next
 </template>
-
 <script lang="ts" setup>
+import { computed } from "vue";
 // @ts-ignore
-import {
-  paginatedData,
-  dataArray,
-  perPage,
-  page,
-  nextPage,
-  backPage,
-  goToPage,
-} from "../composable/pagination.js";
+import { paginatedData } from "../composable/pagination.js";
 </script>
-
 <style lang="scss">
 .pagination {
-  padding: 100px;
+  padding: 20px;
   text-align: center;
-  div {
-    padding: 20px;
-  }
-  button {
-    font-family: "Poppins";
-    font-style: normal;
-    font-weight: 600;
-    font-size: 18px;
-    line-height: 24px;
-    color: #000000;
-    border: none;
-    background: transparent;
-    cursor: pointer;
-    height: 40px;
-  }
-  button.prev {
-    margin-right: 20px;
-  }
-  button.next {
-    margin-left: 20px;
-  }
-  button.prev::before {
-    display: inline-block;
-    content: "";
-    width: 15px;
-    height: 12px;
-    margin-right: 10px;
-    background-image: url("../assets/prev.svg");
-    background-repeat: no-repeat;
-  }
-  button.next::after {
-    display: inline-block;
-    content: "";
-    width: 15px;
-    height: 12px;
-    margin-left: 10px;
-    background-image: url("../assets/next.svg");
-    background-repeat: no-repeat;
-  }
-  button.item {
-    font-family: "Poppins";
-    font-style: normal;
-    font-weight: 600;
-    font-size: 18px;
-    line-height: 130.02%;
-    color: #959b9d;
-    margin: 0 16px;
-    &:hover,
-    :focus,
-    :active {
-      width: 40px;
-      height: 40px;
-      background: #060606;
-      border-radius: 68px;
-      color: #fff;
-    }
-  }
 }
 </style>
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 PaginationList
+PaginationComponent
 </template>
 
 <script lang="ts" setup>
 import PaginationList from "@/components/PaginationList.vue";
-
-const components = { PaginationList };
+import PaginationComponent from "@/components/PaginationComponent.vue";
+const components = { PaginationList, PaginationComponent };
 </script>
